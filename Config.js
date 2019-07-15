f0e80292f0cc7d0e08dcc47f9538ae5639fcc158
Omar Sow <omarwsow@gmail.com>
2019-07-15 13:14:59 -0400

    split of keys and config files

---
+++ Config.js
@@ -0,0 +1,13 @@
+var LIVE_MODE = true
+
+var LIVE_SOD = 10 //in 24 hr time, the earliest hour we can make calls
+
+var LIVE_EOD = 18 //in 24 hr time, the last hour we can make calls
+
+var MINUTES_BACK_FOR_QUEUE = 5 //look this many minutes back for queued events to check, can be increased, probably shouldn't be decreased
+
+var CONTACTS_CAP = 4 //cap on number of contacts with a particular address/phone in a day, not yet impletemneted
+
+var TWILIO_CONCURRENCY_LIMIT = 100 //limit on how many concurrent tasks we can have stored
+
+var PER_PATIENT_EVENT_LIMIT = 4
