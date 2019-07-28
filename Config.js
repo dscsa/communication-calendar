6c5f8ee4d5d778fd766e081c75c7a4f47b6d2757
akircher <adam@Adams-MacBook-Pro-2.local>
2019-07-28 18:08:00 -0400

    7pm cutoff time

--- Config.js
+++ Config.js
@@ -1,6 +1,6 @@
 var LIVE_SOD = 10 //in 24 hr time, the earliest hour we can make calls
 
-var LIVE_EOD = 18 //in 24 hr time, the last hour we can make calls
+var LIVE_EOD = 19 //in 24 hr time, the last hour we can make calls
 
 var MINUTES_BACK_FOR_QUEUE = 5 //look this many minutes back for queued events to check, can be increased, probably shouldn't be decreased
 
